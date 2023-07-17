c239f3df2a47bea1b9c6dda3968d994af1cd4801
SP3CK <crtosp3ck@gmail.com>
2023-07-18 02:16:07 +0800

    Sales list page

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -46,7 +48,7 @@ func main() {
 
 	// POST New Sale
 	app.Post("/main/new-sale/", func(c *fiber.Ctx) error {
-		ns := new(NewSaleFormData)
+		ns := new(FormDataNewSale)
 		if err := c.BodyParser(ns); err != nil {
 			return err
 		}
@@ -55,7 +57,7 @@ func main() {
 		var qty int
 		var url string
 		var itemId int
-		paymentType, _ := parsePaymentMethod(ns.PaymentMethod)
+		paymentType, _ := parsePaymentMethodToInt(ns.PaymentMethod)
 		operationId := 1
 
 		//i need to change DB structure to accommodate ever growing product list.
@@ -106,8 +108,104 @@ func main() {
 	})
 
 	app.Get("/main/sales-history", func(c *fiber.Ctx) error {
+		url := apiServerAddr + "sales/find/"
+
+		client := http.Client{
+			Timeout: time.Second * 2, // Timeout after 2 seconds
+		}
+
+		req, err := http.NewRequest(http.MethodGet, url, nil)
+		if err != nil {
+			log.Println("Post request not completed -", err)
+			//redirect back to /main/new-sale w/ toast saying error occured
+			return c.Redirect("/main/sales-history")
+		}
+
+		req.Header.Set("User-Agent", "mims-app")
+		res, err := client.Do(req)
+		if err != nil {
+			log.Println("Error occured while awaiting response -", err)
+			//redirect back to /main/new-sale w/ toast saying error occured
+			return c.Redirect("/main/sales-history")
+		}
+
+		if res.Body != nil {
+			defer res.Body.Close()
+		}
+
+		body, err := ioutil.ReadAll(res.Body)
+		_ = body
+		if err != nil {
+			log.Println("Error reading response body -", err)
+			//redirect back to /main/new-sale w/ toast saying error occured
+			return c.Redirect("/main/sales-history")
+		}
+
+		var jsonSales struct {
+			Sales []JsonSale `json:"sales"`
+		}
+
+		//convert that string (body) to json
+		if err := json.Unmarshal(body, &jsonSales); err != nil {
+			log.Println("Error unmarshalling body into JSON -", err)
+			//redirect back to /main/new-sale w/ toast saying error occured
+			return c.Redirect("/main/sales-history")
+		}
+
+		//create an array of view sales with that json information..
+		viewSales := []*ViewSale{}
+
+		for index := range jsonSales.Sales {
+			//parsing some stuff before hand
+			paymentType, _ := parsePaymentMethodToString(jsonSales.Sales[index].PaymentType)
+			operation, _ := parseOperationToString(jsonSales.Sales[index].OperationID)
+			item, _ := parseItemToString(jsonSales.Sales[index].ItemID)
+			time := strings.Split(strings.Split(jsonSales.Sales[index].CreatedAt.String(), " ")[1], ".")[0]
+			date := strings.Split(jsonSales.Sales[index].CreatedAt.String(), " ")[0]
+
+			viewSale := ViewSale{
+				ID:          jsonSales.Sales[index].ID,
+				Amount:      "RM" + strconv.FormatFloat(float64(jsonSales.Sales[index].Amount), 'f', -1, 64),
+				Qty:         strconv.FormatFloat(float64(jsonSales.Sales[index].Amount), 'f', -1, 64) + " unit(s)",
+				PaymentType: paymentType,
+				Operation:   operation,
+				Item:        item,
+				Time:        time,
+				Date:        date,
+			}
+			viewSales = append(viewSales, &viewSale)
+		}
+
+		viewSales = reverseSales(viewSales)
+
+		//**Test sales data to see if it renders properly via the html template**
+		// sales := []Sale{
+		// 	{
+		// 		ID:          1,
+		// 		Amount:      16.00,
+		// 		Qty:         2,
+		// 		PaymentType: 1,
+		// 		OperationID: 1,
+		// 		ItemID:      1,
+		// 		CreatedAt:   time.Now().Truncate(time.Duration(time.Now().Day())),
+		// 		UpdatedAt:   time.Now().Truncate(time.Duration(time.Now().Day())),
+		// 	},
+		// 	{
+		// 		ID:          2,
+		// 		Amount:      24.00,
+		// 		Qty:         3,
+		// 		PaymentType: 2,
+		// 		OperationID: 1,
+		// 		ItemID:      1,
+		// 		CreatedAt:   time.Now().Truncate(time.Duration(time.Now().Day())),
+		// 		UpdatedAt:   time.Now().Truncate(time.Duration(time.Now().Day())),
+		// 	},
+		// }
+
+		//pass it to the renderer
 		return c.Render("sales-history", fiber.Map{
 			"Title": "Sales History",
+			"Sales": viewSales,
 		}, "layouts/main")
 	})
 
@@ -118,7 +216,14 @@ func main() {
 	log.Fatal(app.Listen(":3000"))
 }
 
-func parsePaymentMethod(paymentType string) (int, error) {
+func reverseSales(input []*ViewSale) []*ViewSale {
+	if len(input) == 0 {
+		return input
+	}
+	return append(reverseSales(input[1:]), input[0])
+}
+
+func parsePaymentMethodToInt(paymentType string) (int, error) {
 	switch {
 	case paymentType == "Cash":
 		return 1, nil
@@ -133,10 +238,69 @@ func parsePaymentMethod(paymentType string) (int, error) {
 	}
 }
 
-type NewSaleFormData struct {
+func parsePaymentMethodToString(paymentType int) (string, error) {
+	switch {
+	case paymentType == 1:
+		return "Cash", nil
+	case paymentType == 2:
+		return "QR - Maybank", nil
+	case paymentType == 3:
+		return "QR - Touch & Go", nil
+	case paymentType == 99:
+		return "Free", nil
+	default:
+		return "", errors.New("Unable to parse payment method")
+	}
+}
+
+func parseOperationToString(operationId int) (string, error) {
+	switch {
+	case operationId == 1:
+		return "Kebun Che Mah, Kemensah", nil
+	default:
+		return "", errors.New("Unable to parse operation id")
+	}
+}
+
+func parseItemToString(itemId int) (string, error) {
+	switch {
+	case itemId == 1:
+		return "MD2 Cold Pressed", nil
+	case itemId == 2:
+		return "MD2 Fresh Cut Fruit", nil
+	case itemId == 3:
+		return "MD2 Raw Fruit", nil
+	default:
+		return "", errors.New("Unable to parse item id")
+	}
+}
+
+type FormDataNewSale struct {
 	Sale_TimeDate  string `json:"sale_time_date" xml:"sale_time_date" form:"sale_time_date"`
 	PaymentMethod  string `json:"payment_type" xml:"payment_type" form:"payment_type"`
 	Qty_FreshJuice int    `json:"fresh_juice_qty" xml:"fresh_juice_qty" form:"fresh_juice_qty"`
 	Qty_CutFruit   int    `json:"cut_fruit_qty" xml:"cut_fruit_qty" form:"cut_fruit_qty"`
 	Qty_RawFruit   int    `json:"raw_fruit_qty" xml:"raw_fruit_qty" form:"raw_fruit_qty"`
+}
+
+type JsonSale struct {
+	ID          int       `json:"id"`
+	Amount      float32   `json:"amount"`
+	Qty         float32   `json:"quantity"` //this is float and not int bcos in case we plan to sell by weight, then it wouldnt make sense to use int
+	PaymentType int       `json:"payment_type"`
+	OperationID int       `json:"operation_id"`
+	ItemID      int       `json:"item_id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type ViewSale struct {
+	ID          int    `json:"id"`
+	Amount      string `json:"amount"`
+	Qty         string `json:"quantity"` //this is float and not int bcos in case we plan to sell by weight, then it wouldnt make sense to use int
+	PaymentType string `json:"payment_type"`
+	Operation   string `json:"operation"`
+	Item        string `json:"item"`
+	Time        string `json:"time"`
+	Date        string `json:"date"`
 }
